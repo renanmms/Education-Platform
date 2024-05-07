7d05ea2df74de5465ff00512a464ff8497e9bdb9
Renan Martins <renanmartins1999@hotmail.com>
2024-05-07 19:36:22 -0300

    feat: dependency injection of db context

--- src/EducationPlatform.API/Program.cs
+++ src/EducationPlatform.API/Program.cs
@@ -1,3 +1,6 @@
+using EducationPlatform.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("EducationPlatformCS");
+builder.Services.AddDbContext<EducationPlatformDbContext>(
+    options => options.UseNpgsql(connectionString)
+);
 
 var app = builder.Build();
 
