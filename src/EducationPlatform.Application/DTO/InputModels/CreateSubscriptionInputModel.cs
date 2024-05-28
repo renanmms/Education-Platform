ebe0cac46a111510183ab7df1c5efdd9efd56fd7
Renan Martins <renanmartins1999@hotmail.com>
2024-05-27 22:54:07 -0300

    feat: create subscription command

---
+++ src/EducationPlatform.Application/DTO/InputModels/CreateSubscriptionInputModel.cs
@@ -0,0 +1,8 @@
+namespace EducationPlatform.Application.DTO.InputModels
+{
+    public class CreateSubscriptionInputModel
+    {
+        public string? Name { get; set; }
+        public int Duration { get; set; }
+    }
+}
