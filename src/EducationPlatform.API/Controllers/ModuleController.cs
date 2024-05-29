a2b83b05f336112cbe8f0f56c0826277869756e4
Renan Martins <renanmartins1999@hotmail.com>
2024-05-28 21:50:29 -0300

    feat: module controller

---
+++ src/EducationPlatform.API/Controllers/ModuleController.cs
@@ -0,0 +1,35 @@
+using System.Runtime.CompilerServices;
+using EducationPlatform.Application.Commands.CreateModule;
+using EducationPlatform.Application.DTO.InputModels;
+using EducationPlatform.Application.Queries.GetModule;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EducationPlatform.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ModuleController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public ModuleController(IMediator mediator)
+        {
+            _mediator = mediator;    
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var query = new GetModuleQuery(id);
+            var module = await _mediator.Send(query);
+            return Ok(module);
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> Create(CreateModuleCommand command)
+        {
+            var id = await _mediator.Send(command);
+            return CreatedAtAction(nameof(GetById), new {id = id}, command);
+        }
+    }
+}
