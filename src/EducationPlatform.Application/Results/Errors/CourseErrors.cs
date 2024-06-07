23df0cadfd3f92755b16d576b97c88ac46fa481b
Renan Martins <renanmartins1999@hotmail.com>
2024-06-07 11:15:11 -0300

    feat: adding errors

---
+++ src/EducationPlatform.Application/Results/Errors/CourseErrors.cs
@@ -0,0 +1,7 @@
+namespace EducationPlatform.Application.Results.Errors
+{
+    public static class CourseErrors
+    {
+        public static readonly Error CourseNotFound = new("Courses.CourseNotFound", "Course not founded.");
+    }
+}
---
+++ src/EducationPlatform.Application/Results/Errors/ModuleErrors.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace EducationPlatform.Application.Results.Errors
+{
+    public class ModuleErrors
+    {
+        
+    }
+}
