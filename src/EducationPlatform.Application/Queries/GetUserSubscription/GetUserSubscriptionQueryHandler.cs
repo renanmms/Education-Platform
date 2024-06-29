65abec3598143d4d47457716c18a4d1a6dd647a4
Renan Martins <renanmartins1999@hotmail.com>
2024-06-29 00:43:26 -0300

    feat: get user subscription query and dto

---
+++ src/EducationPlatform.Application/Queries/GetUserSubscription/GetUserSubscriptionQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using EducationPlatform.Application.DTO.ViewModels;
+using EducationPlatform.Core.Interfaces.Repositories;
+using MediatR;
+
+namespace EducationPlatform.Application.Queries.GetUserSubscription
+{
+    public class GetUserSubscriptionQueryHandler : IRequestHandler<GetUserSubscriptionQuery, GetUserSubscriptionViewModel>
+    {
+        private readonly IUserRepository _repository;
+        public GetUserSubscriptionQueryHandler(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetUserSubscriptionViewModel> Handle(GetUserSubscriptionQuery request, CancellationToken cancellationToken)
+        {
+            var userSubscription = await _repository.GetUserSubscriptionAsync(request.UserId, request.SubscriptionId);
+
+            return new GetUserSubscriptionViewModel(userSubscription.UserId,
+                userSubscription.User.Fullname,
+                userSubscription.User.Email,
+                userSubscription.SubscriptionId,
+                userSubscription.Subscription?.Name,
+                userSubscription.StartDate,
+                userSubscription.ExpirationDate);
+        }
+    }
+}
