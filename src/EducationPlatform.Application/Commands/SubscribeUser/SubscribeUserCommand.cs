e78f123a660feafae87f9bf74904d7151db660d6
Renan Martins <renanmartins1999@hotmail.com>
2024-06-28 23:50:43 -0300

    feat: add subscribe user command

---
+++ src/EducationPlatform.Application/Commands/SubscribeUser/SubscribeUserCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace EducationPlatform.Application.Commands.SubscribeUser
+{
+    public record SubscribeUserCommand(
+        Guid UserId,
+        Guid SubscriptionId,
+        DateTime StartDate,
+        DateTime ExpirationDate
+    ) : IRequest<Guid>;
+}
