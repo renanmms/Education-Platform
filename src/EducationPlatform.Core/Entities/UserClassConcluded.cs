abe071ed2493f2d56f61699710e112f75d72b1cc
Renan Martins <renanmartins1999@hotmail.com>
2024-06-28 23:13:25 -0300

    feat: constructor for entity

--- src/EducationPlatform.Core/Entities/UserClassConcluded.cs
+++ src/EducationPlatform.Core/Entities/UserClassConcluded.cs
@@ -2,6 +2,14 @@ namespace EducationPlatform.Core.Entities
 {
     public class UserClassConcluded
     {
+        public UserClassConcluded(Guid userId, Guid classId, DateTime conclusionDate, int score)
+        {
+            UserId = userId;
+            ClassId = classId;
+            ConclusionDate = conclusionDate;
+            Score = score;
+        }
+
         public Guid Id { get; set; }
         public Guid UserId { get; set; }
         public Guid ClassId { get; set; }
