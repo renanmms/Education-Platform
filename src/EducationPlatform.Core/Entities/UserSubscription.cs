dd033c5b2745cac9fda26bbf08c359a908097340
Renan Martins <renanmartins1999@hotmail.com>
2024-06-28 23:49:44 -0300

    feat: constructor for user subscription

--- src/EducationPlatform.Core/Entities/UserSubscription.cs
+++ src/EducationPlatform.Core/Entities/UserSubscription.cs
@@ -2,13 +2,21 @@ namespace EducationPlatform.Core.Entities
 {
     public class UserSubscription
     {
+        public UserSubscription(Guid userId, Guid subscriptionId, DateTime startDate, DateTime expirationDate)
+        {
+            UserId = userId;
+            SubscriptionId = subscriptionId;
+            StartDate = startDate;
+            ExpirationDate = expirationDate;
+        }
+
         public Guid Id { get; set; }
         public Guid UserId { get; set; }
         public Guid SubscriptionId { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime ExpirationDate { get; set; }
+        public User User { get; set; } = null!;
         public Subscription? Subscription { get; set; }
         public PaymentSubscription? PaymentSubscription { get; set; }
-        public User User { get; set; } = null!;
     }
 }
