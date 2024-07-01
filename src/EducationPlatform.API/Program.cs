71f732086047ea6b4099a5cfab665c6c176bf3d2
Renan Martins <renanmartins1999@hotmail.com>
2024-06-30 23:00:10 -0300

    feat: always use swagger

--- src/EducationPlatform.API/Program.cs
+++ src/EducationPlatform.API/Program.cs
@@ -30,11 +30,10 @@ builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<Cr
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
