8c000cb72cb5931d2ca91302f2d1734ceebbd92e
Renan Martins <renanmartins1999@hotmail.com>
2024-07-16 22:04:57 -0300

    feat: generating migrations

---
+++ src/EducationPlatform.Infrastructure/Persistence/Migrations/20240717004542_WithManyUserSubscriptions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EducationPlatform.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class WithManyUserSubscriptions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_UserSubscriptions_SubscriptionId",
+                table: "UserSubscriptions");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserSubscriptions_SubscriptionId",
+                table: "UserSubscriptions",
+                column: "SubscriptionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_UserSubscriptions_SubscriptionId",
+                table: "UserSubscriptions");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserSubscriptions_SubscriptionId",
+                table: "UserSubscriptions",
+                column: "SubscriptionId",
+                unique: true);
+        }
+    }
+}
